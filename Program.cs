6a70c3b316d1086afb0241d7e2a3dea4408a89e4
Kevin Arsenault <arsenaultk9@gmail.com>
2018-05-09 15:03:05 -0400

    Example for work.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.ClearScript.JavaScript;
+using Microsoft.ClearScript.V8;
 
 namespace CScharpExecuteJavascript
 {
     class Program
     {
+        // Full doc: https://github.com/Microsoft/ClearScript
         static void Main(string[] args)
         {
+            var taskA = new Task("TaskA", 54, 0.74m, 0.24m);
+            var taskB = new Task("TaskB", 72, 0.74m, 0.24m);
+
+            // create a script engine
+            using (var engine = new V8ScriptEngine())
+            {
+                const string evaluateSetupJsFunction = @"
+                function getSetup(previousTask, currentTask) {
+                    var previousTaskSetup = previousTask.Quantity * previousTask.BeeingFirstPenaltyFactor;
+                    var currentTaskSetup = currentTask.Quantity * currentTask.BeeingSecondPenaltyFactor;
+
+                    return {
+                        previousTaskSetup: previousTaskSetup,
+                        currentTaskSetup: currentTaskSetup
+                    }
+                }";
+
+                // call a script function
+                engine.Execute(evaluateSetupJsFunction);
+                var setupTimes = engine.Script.getSetup(taskA, taskB);
+
+                Console.WriteLine($"{ taskA.Number } setup time: {setupTimes.previousTaskSetup}h");
+                Console.WriteLine($"{ taskB.Number } setup time: {setupTimes.currentTaskSetup}h");
+
+            }
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
---
+++ Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScharpExecuteJavascript
+{
+    public class Task
+    {
+        public Task(string number, int quantity, decimal beeingFirstPenaltyFactor, decimal beeingSecondPenaltyFactor)
+        {
+            Number = number;
+            Quantity = quantity;
+            BeeingFirstPenaltyFactor = beeingFirstPenaltyFactor;
+            BeeingSecondPenaltyFactor = beeingSecondPenaltyFactor;
+        }
+
+        public string Number { get; }
+        public int Quantity { get; }
+        public decimal BeeingFirstPenaltyFactor { get; }
+        public decimal BeeingSecondPenaltyFactor { get; }
+    }
+}
